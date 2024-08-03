55e18f3d6012e037792cbbd4c9b4d4650ffbd837
Lokendra Kumar Agrawal <erlokendra.agrawal@gmail.com>
2024-08-03 15:30:33 +0530

    added project heading

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+st.write("""
+# Employee Turnover Analysis using AI and Machine Learning
+# Developed by Mr. Lokendra Kumar Agrawal
 
-print("main function started !!!!!!!!!1")
-
-st.text("main function")
+""")
